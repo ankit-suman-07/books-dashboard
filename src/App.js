132d8b7434c334771a52cb2f6a5f87dd0fa81d9b
ankit-suman-07 <ankitsuman07@gmail.com>
2024-05-22 05:35:16 +0530

    css: navbar design completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./Navbar.css";
 
+import LinkedInIcon from "../../assets/linkedin.png";
+import GitHubIcon from "../../assets/github.png";
+import CodeIcon from "../../assets/code.png";
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar' >
+            <div className='nav-logo' >
+                BOOKS.
+            </div>
+            <div className='nav-socials' >
+                <a href='https://github.com/ankit-suman-07/books-dashboard' >
+                    <img src={CodeIcon} alt='code-icon' />
+                </a>
+                <a href='https://github.com/ankit-suman-07' >
+                    <img src={GitHubIcon} alt='github-icon' />
+                </a>
+                <a href='https://www.linkedin.com/in/ankit--suman/' >
+                    <img src={LinkedInIcon} alt='linked-in-icon' />
+                </a>
+            </div>
+        </div>
     )
 }
 
